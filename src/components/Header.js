1f8af3d5e358dadd9baea7a95e030824c358a6b5
Waldemar Głaz <w.glaz@cheil.com>
2022-11-08 13:10:35 +0100

    images folder added

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+function Header() {
+  return <header className={classes.header}>header</header>;
+}
+
+export default Header;
