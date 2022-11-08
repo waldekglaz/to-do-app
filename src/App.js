c11909e25ee4d378700b6989ed59007fba477711
Waldemar Głaz <w.glaz@cheil.com>
2022-11-08 13:39:04 +0100

    header with theme toggle connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
+import useLocalStorage from "use-local-storage";
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+  const themeToggleHandler = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
-    <div className="App">
-      <Header />
+    <div className="App" data-theme={theme}>
+      <Header onThemeToggle={themeToggleHandler} theme={theme} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,27 @@ import classes from "./Header.module.css";
 import { IconContext } from "react-icons";
 import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 
-function Header(props) {
+function Header({ onThemeToggle, theme }) {
   return (
     <header className={classes.header}>
       <div className={classes.container}>
         <h1>TODO</h1>
-        <IconContext.Provider value={{ size: "20px" }}>
-          <BsFillMoonFill
-            style={{ cursor: "pointer" }}
-            onClick={props.onThemeToggle}
-          />
-        </IconContext.Provider>
+        {theme === "light" && (
+          <IconContext.Provider value={{ size: "20px" }}>
+            <BsFillMoonFill
+              style={{ cursor: "pointer" }}
+              onClick={onThemeToggle}
+            />
+          </IconContext.Provider>
+        )}
+        {theme === "dark" && (
+          <IconContext.Provider value={{ size: "20px" }}>
+            <BsFillSunFill
+              style={{ cursor: "pointer" }}
+              onClick={onThemeToggle}
+            />
+          </IconContext.Provider>
+        )}
       </div>
     </header>
   );
