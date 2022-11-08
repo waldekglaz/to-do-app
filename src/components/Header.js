3de24f98a01fd729a14fcfdefbd19d48fc97eb53
Waldemar Głaz <w.glaz@cheil.com>
2022-11-08 13:31:28 +0100

    install use-local-storage and react-icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
 import classes from "./Header.module.css";
+import { IconContext } from "react-icons";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 
-function Header() {
-  return <header className={classes.header}>header</header>;
+function Header(props) {
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <h1>TODO</h1>
+        <IconContext.Provider value={{ size: "20px" }}>
+          <BsFillMoonFill
+            style={{ cursor: "pointer" }}
+            onClick={props.onThemeToggle}
+          />
+        </IconContext.Provider>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
